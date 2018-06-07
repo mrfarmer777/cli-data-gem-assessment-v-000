b8541c3e8b40a35907983d00b64b7f4f33f472c0
Matt Farmer <mrfarmer777@gmail.com>
2018-06-07 15:55:27 +0000

    automatically backed up by learn

--- tripadCLIsor/lib/tripadCLIsor.rb
+++ tripadCLIsor/lib/tripadCLIsor.rb
@@ -1,13 +1,12 @@
 #this is functioning as our environment
 
-<<<<<<< HEAD
+
 require_relative "./tripadCLIsor/version.rb"
 require_relative "./tripadCLIsor/cli.rb"
 require_relative "./tripadCLIsor/scraper.rb"
-=======
+
 require "tripadCLIsor/version"
 require_relative "./tripadCLIsor/cli"
->>>>>>> 953c40abcb060fc33602e670cfc3640a32a398f9
 
 module TripadCLIsor
   # Your code goes here...
